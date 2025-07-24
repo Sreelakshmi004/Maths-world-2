b97a7dfc5b36334eeaa73c31decaa7fcfbcd712d
Sreelakshmi004 <nairsreelakshmi0042gmail.com>
2025-07-24 17:06:55 +0530

    Upload

---
+++ Assets/Script/Camera.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform Player;
+    public float offset;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 campos = transform.position;
+        campos.z = Player.position.z + offset;
+        transform.position = campos;
+    }
+}
--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -6,48 +6,55 @@ public class PlayerControl : MonoBehaviour
     private Vector3 direction;
     public float forwardspeed;
     private int desiredlane = 1; // 0 = left, 1 = middle, 2 = right
-    public float lanedistance;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float lanedistance = 2.5f;
+    public float jumpforce;
+    public float gravity = -9.18f;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        
         direction.z = forwardspeed;
-        if (Input.GetKey(KeyCode.RightArrow)    )
-        {
-            desiredlane++;
-            if (desiredlane >2)
-            {
-                desiredlane = 2;
-            }
-            
-        }
-        if (Input.GetKey(KeyCode.LeftArrow)) 
+        
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             desiredlane--;
-            if (desiredlane <0)
-            {
-                desiredlane = 0;
-            }
-           
+            if (desiredlane < 0) desiredlane = 0;
+        }
 
-        }
-        Vector3 targetposition = transform.position.z * transform.forward + transform.position.y * transform.up;
-        if (desiredlane == 0)
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            targetposition += Vector3.left * lanedistance;
+            desiredlane++;
+            if (desiredlane > 2) desiredlane = 2;
         }
-        else if (desiredlane == 2)
+        if(controller.isGrounded)
         {
-            targetposition += Vector3.right * lanedistance;
+            if (Input.GetKey(KeyCode.Space))
+            {
+                jump();
+            }
         }
-        transform.position = targetposition;
+        else
+        {
+            direction.y += gravity * Time.deltaTime;
+        }
+       
+
+        Vector3 targetPosition = transform.position.z * Vector3.forward + transform.position.y * Vector3.up;
+        float laneOffset = (desiredlane - 1) * lanedistance;
+        targetPosition += Vector3.right * laneOffset;
+
+        transform.position = targetPosition;
     }
-    private void FixedUpdate()
+    private void jump()
+    {
+        direction.y = jumpforce;
+    }
+    void FixedUpdate()
     {
         controller.Move(direction * Time.fixedDeltaTime);
     }
